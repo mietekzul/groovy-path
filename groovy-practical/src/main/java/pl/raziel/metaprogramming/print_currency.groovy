0658056b4c060e2bf1189b552dea4f59321d444b
go2osta <Diabolo!>
2017-09-15 13:43:01 +0200

    Added meta programming example.

---
+++ groovy-practical/src/main/java/pl/raziel/metaprogramming/print_currency.groovy
@@ -0,0 +1,18 @@
+package pl.raziel.metaprogramming
+
+import java.text.NumberFormat
+
+Number.metaClass.asCurrency = {
+    NumberFormat nf = NumberFormat.getCurrencyInstance()
+    nf.format(delegate)
+}
+
+Number.metaClass.asCurrency = {
+    Locale loc ->
+        NumberFormat nf = NumberFormat.getCurrencyInstance(loc)
+        nf.format(delegate)
+}
+
+def amount = 123456.7890
+Locale.default = Locale.US
+assert amount.asCurrency() == '$123,456.79'

95cf4fe60da76e65cc1f884058de53cb4e8adcdb
go2osta <Diabolo!>
2017-09-18 14:43:52 +0200

    I/O

---
+++ groovy-practical/src/main/java/pl/raziel/io/sum_numbers.groovy
@@ -0,0 +1,8 @@
+package pl.raziel.io
+
+println 'Please enter some numbers to sum'
+int total = 0
+System.in.withReader { br ->
+    total = br.readLine().tokenize()*.toInteger().sum()
+}
+println "The total is $total"
---
+++ groovy-practical/src/main/java/pl/raziel/io/sum_numbers_loop.groovy
@@ -0,0 +1,7 @@
+package pl.raziel.io
+
+println 'Sum numbers with looping'
+System.in.eachLine { line ->
+    if (!line) System.exit(0)
+    println line.split()*.toBigDecimal().sum()
+}

ea1c8770cc9abbf35731cbeaa8ca53e0cb49417e
go2osta <Diabolo!>
2017-09-19 09:25:32 +0200

    Groovy department.

--- groovy-practical/src/main/java/pl/raziel/hr/Department.groovy
+++ groovy-practical/src/main/java/pl/raziel/hr/Department.groovy
@@ -1,4 +1,34 @@
 package pl.raziel.hr
 
 class Department {
+    int id;
+    String name;
+    Map<Integer, Employee> employeeMap = [:]
+
+    void hire(Employee e) {
+        employeeMap[e.id] = e
+    }
+
+    void layOff(Employee e) {
+        empMap.remove(e.id)
+    }
+
+    Collection<Employee> getEmployees() {
+        empMap.values()
+    }
+
+    Department plus(Employee e) {
+        hire(e)
+        this
+    }
+
+    Department minus(Employee e) {
+        layOff(e)
+        this
+    }
+
+    Department leftShift(Employee e) {
+        hire(e)
+        this
+    }
 }

a43939a89909a714f550d44bd7525c2f615ccd23
Daniel Ostapiuk <daniel.ostapiuk@commerzsystems.com>
2017-09-28 14:43:13 +0200

    Added Response.

--- groovy-mastering/groovy-rest/src/main/java/pl/raziel/rest/PersonResource.groovy
+++ groovy-mastering/groovy-rest/src/main/java/pl/raziel/rest/PersonResource.groovy
@@ -1,19 +1,7 @@
 package pl.raziel.rest
 
-import javax.ws.rs.Consumes
-import javax.ws.rs.DELETE
-import javax.ws.rs.GET
-import javax.ws.rs.POST
-import javax.ws.rs.PUT
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.Produces
-import javax.ws.rs.core.Context
-import javax.ws.rs.core.Link
-import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
-import javax.ws.rs.core.UriBuilder
-import javax.ws.rs.core.UriInfo
+import javax.ws.rs.*
+import javax.ws.rs.core.*
 
 @Path("/people")
 class PersonResource {
@@ -24,8 +12,9 @@ class PersonResource {
 
     @GET
     @Produces([MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML])
-    List<Person> findAll() {
-        dao.findAll()
+    Response findAll() {
+        def people = dao.findAll();
+        Response.ok(people).link(uriInfo.requestUri, 'self').build()
     }
 
     @GET
